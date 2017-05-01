4f3721fd0a86d5eb97df6404ce9d6c703e9d209e
Charlie Stopek <cstopek@hotmail.com>
2017-05-01 16:33:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
+
+function searchRepositories() {
+  const searchTerms = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+      const template = Handlebars.compile($('#results-template').html())
+      $('#results').html(template(data))
+    }).fail(error => { displayError() })
+    console.log("hello")
+}
+
+function showCommits(element) {
+  const owner = element.dataset.owner
+  const repo = element.dataset.repository
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, data => {
+    const template = Handlebars.compile($('#commits-template').html())
+    $('#details').html(template(data))
+  }).fail(error => { displayError() })
+}
+
 function handlebarsSetup() {
   //put any handlebars setup in here
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
